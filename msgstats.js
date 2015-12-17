5b58102117cb7f2ed1849db12f2974290ff7dc65
Richard Rodger <richard@ricebridge.com>
2015-12-17 14:54:20 +0000

    correct version of msgstats.js

--- msgstats.js
+++ msgstats.js
@@ -1,5 +1,4 @@
-/* Copyright (c) 2014-2015 Richard Rodger, MIT License */
-"use strict";
+'use strict'
 
 
 var dgram = require('dgram')
@@ -7,18 +6,22 @@ var dgram = require('dgram')
 var stats  = require('rolling-stats')
 var influx = require('influx')
 
-
-
 module.exports = function msgstats( options ) {
   var seneca = this
   var plugin = 'msgstats'
 
   options = seneca.util.deepextend({
+    tag: seneca.options().tag,
+    pid: process.pid,
     pin: '',
     interval:1000,
     stats:{
       size:1111,
       interval:1000
+    },
+    capture: {
+      mem: true,
+      msg: true
     },
     ratios:[],
     udp:{
@@ -40,8 +43,8 @@ module.exports = function msgstats( options ) {
   var counts    = stats.NamedStats( options.stats.size, options.stats.interval)
   var rstats    = stats.NamedStats( options.stats.size, options.stats.interval)
 
-  
-  seneca.add({init:plugin},function( msg, done ){
+
+  seneca.add({init:plugin}, function(msg, done) {
     var seneca = this
 
     if( options.collect ) {
@@ -50,10 +53,10 @@ module.exports = function msgstats( options ) {
     else {
       var pin  = options.pin || options.pins || null
       var pins = Array.isArray(pin) ? pin : [pin]
-    
+
       pins.forEach(function(pin){
         seneca.sub(pin,function(msg){
-          counts.point(1,msg.meta$.sub)
+          counts.point(1, msg.meta$.sub)
         })
       })
 
@@ -63,15 +66,18 @@ module.exports = function msgstats( options ) {
     done()
   })
 
-  
+
   function start_transmit() {
     setInterval(function(){
-      var latest = counts.calculate()
+      var msg_stats = counts.calculate()
+      var mem_stats = process.memoryUsage()
 
       txrx.transmit({
-        id:    seneca.id,
-        when:  Date.now(),
-        stats: latest
+        pid: options.pid,
+        tag: options.tag,
+        when: Date.now(),
+        msg_stats: msg_stats,
+        mem_stats: mem_stats
       })
     },options.interval)
   }
@@ -84,7 +90,7 @@ module.exports = function msgstats( options ) {
       transmit: function( msg ){
         var data = new Buffer(JSON.stringify(msg))
         client.send(
-          data, 0, data.length, options.udp.port, options.udp.host, 
+          data, 0, data.length, options.udp.port, options.udp.host,
           function(err) {
             if (err) console.log(err)
           })
@@ -110,37 +116,74 @@ module.exports = function msgstats( options ) {
   }
 
 
-  function make_influx_aggregate( msg ) {
+  function make_influx_aggregate (msg) {
     var client = influx(options.influx)
-    
-    return function( msg, ratios ) {
-      var stats = msg.stats || {}
+
+    return function (msg, ratios) {
       var series = {}
-      for( var p in stats ) {
-        series[p]=[[{c:stats[p].sum}]]
+
+      if (options.capture.mem) {
+        var stats = msg.mem_stats || {}
+        series.mem_stats = []
+
+        series.mem_stats.push([
+          {available: stats.heapTotal, used: stats.heapUsed},
+          {pid: msg.pid, tag: msg.tag}
+        ])
       }
-      client.writeSeries(series,function(err){
-        if(err) return console.log(err)
+
+      if (options.capture.msg) {
+        var stats = msg.msg_stats || {}
+        series.msg_stats = []
+
+        for(var pin in stats) {
+          var count = stats[pin].sum
+          pin = pin.replace(',', '_')
+
+          var point = [
+            {count: count},
+            {pin: pin, pid: msg.pid, tag: msg.tag}
+          ]
+
+          series.msg_stats.push(point)
+        }
+      }
+
+      if (Object.keys(ratios)) {
+        for( var rn in ratios ) {
+          var nice_rn = rn.replace(/[,~]/g,'_')
+          
+          var point = [
+            {count: ratios[rn].sum},
+            {pin: nice_rn, pid: msg.pid, tag: msg.tag}
+          ]
+
+          series.msg_stats.push(point)
+
+        }
+      }
+      
+
+      client.writeSeries(series, function (err) {
+        if (err) {
+          return seneca.log.error(err)
+        }
       })
 
-      if( Object.keys(ratios) ) {
-        client.writeSeries(ratios,function(err){
-          if(err) return console.log(err)
-        })
-      }
     }
   }
 
   function make_ratios( aggregate ) {
     return function( msg ) {
-      
+
       if( options.ratios ) {
-        var ratios = {}, z = {sum:null}
+
+        var ratios = {}, z = {count:null}
         for( var i = 0; i < options.ratios.length; i++ ) {
           var ratio = options.ratios[i]
           
-          var r0 = (msg.stats[ratio[0]]||z).sum
-          var r1 = (msg.stats[ratio[1]]||z).sum
+          var r0 = (msg.msg_stats[ratio[0]]||z).count
+          var r1 = (msg.msg_stats[ratio[1]]||z).count
 
           if( null != r0 ) rstats.point(r0,ratio[0])
           if( null != r1 ) rstats.point(r1,ratio[1])
@@ -151,7 +194,7 @@ module.exports = function msgstats( options ) {
         for( var i = 0; i < options.ratios.length; i++ ) {
           var ratio = options.ratios[i]
 
-          var r = (rs[ratio[1]] && rs[ratio[1]].sum && rs[ratio[0]]) ? 
+          var r = (rs[ratio[1]] && rs[ratio[1]].sum && rs[ratio[0]]) ?
                 rs[ratio[0]].sum / rs[ratio[1]].sum : 0
 
 
@@ -159,7 +202,8 @@ module.exports = function msgstats( options ) {
             var rn = ratio[0]+'~'+ratio[1]
             rstats.point(r,rn)
             r = rstats.calculate()[rn].mean
-            ratios[rn]=[[{r:r}]]
+            //ratios[rn]=[[{r:r}]]
+            ratios[rn]={sum:r}
           }
         }
       }
@@ -168,4 +212,3 @@ module.exports = function msgstats( options ) {
     }
   }
 }
-
